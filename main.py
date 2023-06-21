cdb4eb884d93d0870786030ffce3285859f0afe4
Masanari Mizuno <mznms@primu.la>
2023-06-21 22:43:42 +0900

    feat: Add image upload button

--- main.py
+++ main.py
@@ -2,37 +2,45 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from skimage.io import imread
+from PIL import Image
 
 
-# download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
-
-im = imread(img_url)
-
-st.image(im, caption='image from wikimedia commons',
-         use_column_width=True)
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 
-# show histgram of all colors
-hist_red, _ = np.histogram(im[:, :, 0], bins=64)
-hist_green, _ = np.histogram(im[:, :, 1], bins=64)
-hist_blue, _ = np.histogram(im[:, :, 2], bins=64)
-hist = np.stack((hist_red, hist_green, hist_blue), axis=1)
+uploaded_file = st.file_uploader('Choose a image file')
 
-df_hist = pd.DataFrame(hist, columns=['R', 'G', 'B'])
-st.bar_chart(df_hist)
+if uploaded_file is not None:
+    image_pil_array = Image.open(uploaded_file)
+    st.image(
+        image_pil_array, caption='uploaded image',
+        use_column_width=True
+    )
 
 
-# choose one color
-color = st.radio(
-    "choose R, G, or B",
-    ('R', 'G', 'B'))
-if color == 'R':
-    df_hist = pd.DataFrame(hist_red)
+    im = np.array(image_pil_array)
+
+
+    # show histgram of all colors
+    hist_red, _ = np.histogram(im[:, :, 0], bins=64)
+    hist_green, _ = np.histogram(im[:, :, 1], bins=64)
+    hist_blue, _ = np.histogram(im[:, :, 2], bins=64)
+    hist = np.stack((hist_red, hist_green, hist_blue), axis=1)
+
+    df_hist = pd.DataFrame(hist, columns=['R', 'G', 'B'])
     st.bar_chart(df_hist)
-if color == 'G':
-    df_hist = pd.DataFrame(hist_green)
-    st.bar_chart(df_hist)
-if color == 'B':
-    df_hist = pd.DataFrame(hist_blue)
-    st.bar_chart(df_hist)
+
+
+    # choose one color
+    color = st.radio(
+        "choose R, G, or B",
+        ('R', 'G', 'B'))
+    if color == 'R':
+        df_hist = pd.DataFrame(hist_red)
+        st.bar_chart(df_hist)
+    if color == 'G':
+        df_hist = pd.DataFrame(hist_green)
+        st.bar_chart(df_hist)
+    if color == 'B':
+        df_hist = pd.DataFrame(hist_blue)
+        st.bar_chart(df_hist)
